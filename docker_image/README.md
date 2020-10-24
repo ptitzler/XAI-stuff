2558355ed43cddc9010471373d8349d5ca5a459f
Patrick Titzler <ptitzler@us.ibm.com>
2020-10-23 18:29:46 -0700

    updates

---
+++ docker_image/README.md
@@ -0,0 +1,11 @@
+## Preparing the Docker image
+
+### Prerequisites:
+ - Docker
+ - DockerHub ID
+
+### Build and publish the Docker image
+- Customize the variables in `build_docker_image.sh`
+- Run `build_docker_image.sh`
+- Customize the variables in `publish_docker_image.sh`
+- Run `publish_docker_image.sh`
--- docker_image/build_docker_image.sh
+++ docker_image/build_docker_image.sh
@@ -14,4 +14,14 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-docker build -t xai .
+
+set -x
+
+# target Docker image name
+IMG_NAME=xai
+
+# existing Docker image tag
+IMG_TAG=latest
+
+# build Docker image
+docker build -t ${IMG_NAME}:${IMG_TAG} .
